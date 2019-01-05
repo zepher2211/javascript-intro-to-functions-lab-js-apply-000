df5e9a57f26e16037d44232bf72548db17d83f8f
zepher2211 <zepher2211@gmail.com>
2019-01-05 05:04:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+<<<<<<< HEAD
 var uppercase = "YES INDEED!"
  
 uppercase.toUpperCase() === uppercase 
  
 var lowercase = 'I can\'t hear you!'
+=======
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase 
+ 
+var lowercase = 'hello!'
+>>>>>>> 51614096f51b8adf2817cbaed40a1e7cd8f9601d
  
 lowercase.toLowerCase() === lowercase 
  
@@ -24,6 +32,7 @@ mixedCase.toLowerCase() === mixedCase
  
 mixedCase.toUpperCase() === mixedCase
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   var MessageToGrandma = string
   if (MessageToGrandma === MessageToGrandma.toLowerCase())
     return `${lowercase}`
@@ -31,4 +40,10 @@ function sayHiToGrandma(string) {
     return `YES INDEED!`
   if (string == 'I love you, Grandma.')
     return 'I love you, too.'
+=======
+  if (string == string.toLowerCase
+    return 'I can\'t hear you')
+  if (string == string.toUpperCase
+    return
+>>>>>>> 51614096f51b8adf2817cbaed40a1e7cd8f9601d
 }
